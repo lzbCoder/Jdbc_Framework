a78cf6e62fdd37ee7d9eaabbff001f2ffe179539
lzbCoder <1838558217@qq.com>
2021-01-22 15:03:56 +0800

    第二个测试

--- src/test/Test3.java
+++ src/test/Test3.java
@@ -13,12 +13,12 @@ public class Test3 {
         Car car = new Car(null,"马自达20","red",40000);
 //        dao3.save(car);
 //        dao3.delete(27);
-        for(int i=1; i<=20; i++) {
-            List<Car> lists = dao3.findAll();
-            for (Car list : lists) {
-                System.out.println(list);
-            }
-            System.out.println(i+"-----------------------------------------");
-        }
+//        for(int i=1; i<=20; i++) {
+//            List<Car> lists = dao3.findAll();
+//            for (Car list : lists) {
+//                System.out.println(list);
+//            }
+//            System.out.println(i+"-----------------------------------------");
+//        }
     }
 }
